0bb5e6ab474175feb3df24f941d719d8a359638a
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-03 17:05:39 -0400

    estilizado tiltle, e usando children

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,10 @@ import Title from '../src/componetes/title/Title'
 function HomePage () {
   return(
     <div>
-      <Title />
-      <Title />
-      <h1>Hello World</h1>
+      <Title>Um titulo</Title> 
+      <p>Um paragrafo qualquer</p>
+      <Title>Outro titulo</Title>
+      <p>Um paragrafo qualquer</p>
     </div>
   )
 }

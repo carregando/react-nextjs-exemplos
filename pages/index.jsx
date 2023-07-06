88ba55c0b05f8920dcb81be37ba14b288a574a38
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-06 15:20:42 -0400

    criado useState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,17 @@
+import {useState} from 'react'  // useState é usado para mostrar uma variavel para o usuário
 import Head from 'next/head'
 
 import Title from '../src/componetes/title/Title'
 import Subtitle from '../src/componetes/subtitle/Subtitle'
+import Button from '../src/componetes/button/Button'
 
 function HomePage () {
+  const [click, setClick] = useState(0)
+  const handleClick = () => {
+    console.log('= quantidade de clicks')
+    setClick(click + 1)
+  }
+
   return(
     <div>
       <>
@@ -14,6 +22,8 @@ function HomePage () {
        <Subtitle text = "Um Subtítulo"/>
        <Title>Outro titulo</Title>
        <Subtitle text = "Outro Subtítulo"/>
+       <Button onClick={handleClick}>Botão teste</Button>
+       <p>Cliques: {click}</p>
       </>
     </div>
   )

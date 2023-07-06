c0a19f94b0b155be58fd68f4bb332bc5f9947af4
Humberto Ferreira Lemos <joojnow@gmail.com>
2023-07-05 23:10:29 -0400

    importado fonte, e configuração do site

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,20 @@
+import Head from 'next/head'
+
 import Title from '../src/componetes/title/Title'
 import Subtitle from '../src/componetes/subtitle/Subtitle'
 
 function HomePage () {
   return(
     <div>
-      <Title>Um titulo</Title>
-      <Subtitle text = "Um Subtítulo"/>
-      <Title>Outro titulo</Title>
-      <Subtitle text = "Outro Subtítulo"/>
+      <>
+       <Head>
+        <title>Titulo diferente da home page</title>
+       </Head>
+       <Title>Um titulo</Title>
+       <Subtitle text = "Um Subtítulo"/>
+       <Title>Outro titulo</Title>
+       <Subtitle text = "Outro Subtítulo"/>
+      </>
     </div>
   )
 }
